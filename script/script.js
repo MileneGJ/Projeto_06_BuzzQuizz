97edc12b9ac165ef104c10185cbd1f613be69954
Milene <milene.jannetti@gmail.com>
2022-04-20 02:23:31 -0300

    Versão 1 - Implementando lista quizzes e botoes iniciais

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function aparecerTela3(){
+    let tela1 = document.querySelector(".container1")
+    tela1.classList.add("escondido")
+    let tela3 = document.querySelector(".container3")
+    tela3.classList.remove("escondido")
+}
